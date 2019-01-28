e6a9d90364c650b15371a81635670a82f00cca69
Stephannie Chiang <stephanniechiang@gmail.com>
2019-01-28 03:03:09 -0200

    Initial commit

---
+++ app.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+    "database/sql"
+
+    "github.com/gorilla/mux"
+    _ "github.com/lib/pq"
+    "fmt"
+    "log"
+)
+
+type App struct {
+    Router *mux.Router
+    DB     *sql.DB
+}
+
+// func (a *App) Initialize(user, password, dbname string) { }
+
+func (a *App) Initialize(user, password, dbname string) {
+    connectionString :=
+        fmt.Sprintf("user=%s password=%s dbname=%s", user, password, dbname)
+
+    var err error
+    a.DB, err = sql.Open("postgres", connectionString)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    a.Router = mux.NewRouter()
+}
+
+func (a *App) Run(addr string) { }
