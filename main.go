8250167ede0badc6a2b069aef978db82d64a6d3a
Stéphannie Chiang <stephanniechiang@gmail.com>
2019-02-06 05:44:10 -0200

    First Try JWT

--- main.go
+++ main.go
@@ -1,83 +1,36 @@
 package main
 
 import (
-    "net/http"
-    // "net/url"
-    // "log"
-    "fmt"
-    "os"
-    "encoding/json"
-
- //    "github.com/go-session/session"
-	// "gopkg.in/oauth2.v3/errors"
-	// "gopkg.in/oauth2.v3/manage"
-	// "gopkg.in/oauth2.v3/models"
-	// "gopkg.in/oauth2.v3/server"
-	// "gopkg.in/oauth2.v3/store"
+	"github.com/gorilla/mux"
+	"redcoins/app"
+	"os"
+	"fmt"
+	"net/http"
+	// "redcoins/controllers"
 )
 
-const clientID = "3481f956589106d754c6"
-const clientSecret = "68179b61e46a6aa005a4cf8adfe809b3940a0af7"
-
 func main() {
-	// a := App{} 
-    
-	// a.Initialize("root", "moedasvermelhas", "RedCoins")
-	// a.Run(":8080")
-	// http.ListenAndServe(":8080", nil)
 
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+	router := mux.NewRouter()
 
-	// We will be using `httpClient` to make external HTTP requests later in our code
-	httpClient := http.Client{}
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
 
-	// Create a new redirect route route
-	http.HandleFunc("/oauth/redirect", func(w http.ResponseWriter, r *http.Request) {
-		// First, we need to get the value of the `code` query param
-		err := r.ParseForm()
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
-			w.WriteHeader(http.StatusBadRequest)
-		}
-		code := r.FormValue("code")
+	router.Use(app.JwtAuthentication) //attach JWT auth middleware
 
-		// Next, lets for the HTTP request to call the github oauth enpoint
-		// to get our access token
-		reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
-		req, err := http.NewRequest(http.MethodPost, reqURL, nil)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
-			w.WriteHeader(http.StatusBadRequest)
-		}
-		// We set this header since we want the response
-		// as JSON
-		req.Header.Set("accept", "application/json")
+	//router.NotFoundHandler = app.NotFoundHandler
 
-		// Send out the HTTP request
-		res, err := httpClient.Do(req)
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-		defer res.Body.Close()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" //localhost
+	}
 
-		// Parse the request body into the `OAuthAccessResponse` struct
-		var t OAuthAccessResponse
-		if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
-			fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
-			w.WriteHeader(http.StatusBadRequest)
-		}
+	fmt.Println(port)
 
-		// Finally, send a response to redirect the user to the "welcome" page
-		// with the access token
-		w.Header().Set("Location", "/welcome.html?access_token="+t.AccessToken)
-		w.WriteHeader(http.StatusFound)
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-
-type OAuthAccessResponse struct {
-	AccessToken string `json:"access_token"`
+	err := http.ListenAndServe(":" + port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
 }
